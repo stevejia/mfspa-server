34f6e02b1c3b526a449d1939f9ba07e77350beaf
xu.jia <1102263474@qq.com>
2022-02-23 19:11:33 +0800

    commit: 微前端提交

---
+++ src/mfspa/layout/content/index.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import { Layout } from "antd";
+const { Content } = Layout;
+import "./index.less";
+import MfspaRouter from "../../router";
+class MfspaContent extends React.Component<any, any, any> {
+  render() {
+    const { children } = this.props;
+    return (
+      <Content>
+        <MfspaRouter>{children}</MfspaRouter>
+      </Content>
+    );
+  }
+}
+
+export default MfspaContent;
